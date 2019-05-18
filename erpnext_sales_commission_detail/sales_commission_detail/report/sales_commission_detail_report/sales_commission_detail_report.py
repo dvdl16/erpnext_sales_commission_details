e7e0f5fd40c648bd0c39333bd1baae4439efba1f
Dirk van der Laarse <dvanderlaarse@horizonglobal.com>
2019-05-18 19:12:52 +0200

    Fixed SQL

--- erpnext_sales_commission_detail/sales_commission_detail/report/sales_commission_detail_report/sales_commission_detail_report.py
+++ erpnext_sales_commission_detail/sales_commission_detail/report/sales_commission_detail_report/sales_commission_detail_report.py
@@ -99,23 +99,23 @@ def get_invoice_items(filters):
 	return frappe.db.sql("""
 		SELECT
 			t_inv.posting_date,
-		    CASE WHEN t_inv.customer_name IS NULL THEN 'Total for all customers' ELSE t_inv.customer_name END AS 'customer_name',
-		    CASE WHEN t_inv.name IS NULL THEN CONCAT('Total for ',t_inv.customer_name) ELSE t_inv.name END AS 'name',
-		    t_invitem.item_code,
-		    t_invitem.item_name,
-		    t_invitem.qty,
-		    t_invitem.rate,
-		    CASE WHEN t_inv.name IS NULL THEN t_inv.invoice_total ELSE t_invitem.base_amount END AS 'total',
-		    CASE WHEN t_inv.name IS NULL THEN NULL ELSE t_inv.commission_rate END as 'commission_rate',
-		    CASE WHEN t_inv.name IS NULL THEN t_inv.total_commission ELSE (t_invitem.base_amount*t_inv.commission_rate/100) END AS 'total_commission'
-		FROM
-			(SELECT posting_date, customer_name, name, SUM(base_net_total) AS 'invoice_total', sales_partner, commission_rate, SUM(total_commission) AS 'total_commission'
+			CASE WHEN t_inv.customer_name IS NULL THEN 'Total for all customers' ELSE t_inv.customer_name END AS 'customer_name',
+			CASE WHEN t_inv.name IS NULL THEN CONCAT('Total for ',t_inv.customer_name) ELSE t_inv.name END AS 'name',
+			t_invitem.item_code,
+			t_invitem.item_name,
+			t_invitem.qty,
+			t_invitem.rate,
+			CASE WHEN t_inv.name IS NULL THEN t_inv.invoice_total ELSE t_invitem.base_amount END AS 'total',
+			CASE WHEN t_inv.name IS NULL THEN NULL ELSE t_inv.commission_rate END as 'commission_rate',
+			CASE WHEN t_inv.name IS NULL THEN t_inv.total_commission ELSE (t_invitem.base_amount*t_inv.commission_rate/100) END AS 'total_commission'
+		FROM (
+			SELECT posting_date, customer_name, name, SUM(base_net_total) AS 'invoice_total', sales_partner, commission_rate, SUM(total_commission) AS 'total_commission'
 			FROM `tabSales Invoice`
-			GROUP BY customer_name, name WITH ROLLUP LIMIT 100 ) AS t_inv
-		LEFT JOIN `tabSales Invoice Item` AS t_invitem ON t_invitem.parent = t_inv.name
-		WHERE t_inv.sales_partner = '{sales_partner}' AND t_inv.posting_date >= '{from_date}' AND t_inv.posting_date <= '{to_date}' """.format(
-			sales_partner=sales_partner,
-			from_date=from_date,
-			to_date=to_date
+			WHERE sales_partner = '{sales_partner}' AND posting_date >= '{from_date}' AND posting_date <= '{to_date}'
+			GROUP BY customer_name, name WITH ROLLUP LIMIT 100 ) AS t_inv LEFT JOIN `tabSales Invoice Item` AS t_invitem 
+			ON t_invitem.parent = t_inv.name """.format(
+				sales_partner=sales_partner,
+				from_date=from_date,
+				to_date=to_date
 			)
 	)
